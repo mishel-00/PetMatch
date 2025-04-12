b5794e36f7656bb68d8604d886b5563a825c532f
mishel-00 <mishel99ilieva@gmail.com>
2025-04-12 22:10:59 +0200

    intento 7

---
+++ backend/dist/firebase.js
@@ -0,0 +1,21 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+var _a;
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.db = void 0;
+// Esto es la conexión con firebase cloud -- mi base de datos 
+//? Cambios hechos para que funcione el desplique en Railway
+const firebase_admin_1 = __importDefault(require("firebase-admin"));
+const serviceAccount = {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: (_a = process.env.FIREBASE_PRIVATE_KEY) === null || _a === void 0 ? void 0 : _a.replace(/\\n/g, "\n"),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+};
+if (!firebase_admin_1.default.apps.length) {
+    firebase_admin_1.default.initializeApp({
+        credential: firebase_admin_1.default.credential.cert(serviceAccount),
+    });
+}
+exports.db = firebase_admin_1.default.firestore();
