185ca16f5416468d5369f06823152ed1f6cfb61a
Santiago1398 <santiagoberrioquintero@gmail.com>
2025-04-13 12:06:09 +0200

    prueba para que funciona por favor

--- frontend/config/apiconfig.ts
+++ frontend/config/apiconfig.ts
@@ -1,16 +1,15 @@
 import Constants from 'expo-constants';
 
-
-
-
 interface ExpoExtra {
-    apiUrl: string;
-    router: {
-        origin: boolean;
-    };
+  apiUrl: string;
+  router: {
+    origin: boolean;
+  };
 }
 
-const extra = ((Constants.expoConfig as any)?.extra as ExpoExtra) ||
-    ((Constants.manifest as any)?.extra as ExpoExtra);
+const extra = ((Constants.expoConfig as any)?.extra as ExpoExtra) 
 
 export const API_URL = extra.apiUrl;
+
+console.log("🚀 API_URL ==>", API_URL);
+
