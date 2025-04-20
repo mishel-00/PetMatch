b5800b45c48fd4180136423c3d24eaad3f61cc70
Santiago1398 <santiagoberrioquintero@gmail.com>
2025-04-20 17:05:14 +0200

    funciona el post y el get para crear animales y se refleje en la lsiat de animales y coorectamente se muestra la foto

--- backend/src/routes/animal.ts
+++ backend/src/routes/animal.ts
@@ -4,6 +4,123 @@ import { ESTADOS_ADOPCION } from "../utils/enums";
 import { verificarTokenFireBase } from "../middleware/verficarTokenFireBase";
 
 const router = express.Router();
+//! Ruta general sin filtros aplicados
+router.get ("/animal", verificarTokenFireBase, async (req, res) => {
+  const uidAsociacion = req.uid;
+  if (!uidAsociacion) {
+    res.status(401).json({ error: "Token inválido" });
+    return;
+  }
+
+  try {
+    const snapshot = await admin
+    .firestore()
+    .collection("animal")
+    .where("asociacion_id", "==", uidAsociacion)
+    .get();
+    console.log("🐾 Animales encontrados:", snapshot.docs.length);
+    const animales = snapshot.docs.map((doc) => ({ 
+        id: doc.id, 
+        ...doc.data() 
+    }));
+    res.status(200).json(animales);
+  } catch (error: any) {
+    console.error("❌ Error al obtener animales:", error);
+    res.status(500).json({ error: error.message });
+    return;
+  }
+});
+
+//! GET -> Filtrar por estado de adopcion
+router.get("/animal/estado", verificarTokenFireBase, async (req, res) => {
+  const uidAsociacion = req.uid;
+  if (!uidAsociacion) {
+    res.status(401).json({ error: "Token inválido" });
+    return;
+  }
+
+  const { estado } = req.query;
+
+  try {
+    const snapshot = await admin
+      .firestore()
+      .collection("animal")
+      .where("asociacion_id", "==", uidAsociacion)
+      .where("estadoAdopcion", "==", estado)
+      .get();
+    console.log("🐾 Animales encontrados:", snapshot.docs.length);
+    const animales = snapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    res.status(200).json(animales);
+  } catch (error: any) {
+    console.error("❌ Error al obtener animales:", error);
+    res.status(500).json({ error: error.message });
+    return;
+  }
+});
+
+//! GET -> Filtrar por especie
+router.get("/animal/especie", verificarTokenFireBase, async (req, res) => {
+  const uidAsociacion = req.uid;
+  if (!uidAsociacion) {
+    res.status(401).json({ error: "Token inválido" });
+    return;
+  }
+
+  const { especie } = req.query;
+
+  try {
+    const snapshot = await admin
+      .firestore()
+      .collection("animal")
+      .where("asociacion_id", "==", uidAsociacion)
+      .where("especie", "==", especie)
+      .get();
+    console.log("🐾 Animales encontrados:", snapshot.docs.length);
+    const animales = snapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    res.status(200).json(animales);
+  } catch (error: any) {
+    console.error("❌ Error al obtener animales:", error);
+    res.status(500).json({ error: error.message });
+    return;
+  }
+});
+
+//! GET -> Filtrar por tipo de raza
+router.get("/animal/tipoRaza", verificarTokenFireBase, async (req, res) => {
+  const uidAsociacion = req.uid;
+  if (!uidAsociacion) {
+    res.status(401).json({ error: "Token inválido" });
+    return;
+  }
+
+  const { tipoRaza } = req.query;
+
+  try {
+    const snapshot = await admin
+      .firestore()
+      .collection("animal")
+      .where("asociacion_id", "==", uidAsociacion)
+      .where("tipoRaza", "==", tipoRaza)
+      .get();
+    console.log("🐾 Animales encontrados:", snapshot.docs.length);
+    const animales = snapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    res.status(200).json(animales);
+  } catch (error: any) {
+    console.error("❌ Error al obtener animales:", error);
+    res.status(500).json({ error: error.message });
+    return;
+  }
+});
+
 
 router.post("/animal", verificarTokenFireBase, async (req, res) => {
 
