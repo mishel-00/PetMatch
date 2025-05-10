ce469aa872ddc23031b34e606cd079c49f7256eb
Santiago1398 <santiagoberrioquintero@gmail.com>
2025-05-10 15:10:24 +0200

    prueba generar codigo QR

--- backend/dist/firebase.js
+++ backend/dist/firebase.js
@@ -11,12 +11,13 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const serviceAccount = {
     projectId: process.env.FIREBASE_PROJECT_ID,
-    privateKey: (_a = process.env.FIREBASE_PRIVATE_KEY) === null || _a === void 0 ? void 0 : _a.replace(/\\n/g, "\n"),
+    privateKey: (_a = process.env.FIREBASE_PRIVATE_KEY) === null || _a === void 0 ? void 0 : _a.replace(/\n/g, "\n"),
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
 };
 if (!firebase_admin_1.default.apps.length) {
     firebase_admin_1.default.initializeApp({
         credential: firebase_admin_1.default.credential.cert(serviceAccount),
+        storageBucket: "pet-match-cloud.appspot.com",
     });
 }
 exports.db = firebase_admin_1.default.firestore();
--- backend/src/firebase.ts
+++ backend/src/firebase.ts
@@ -1,19 +1,20 @@
- // Esto es la conexión con firebase cloud -- mi base de datos 
- import admin from "firebase-admin";
- import dotenv from 'dotenv';
- 
-  dotenv.config();
- const serviceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+// Esto es la conexión con firebase cloud -- mi base de datos 
+import admin from "firebase-admin";
+import dotenv from 'dotenv';
+
+ dotenv.config();
+const serviceAccount = {
+ projectId: process.env.FIREBASE_PROJECT_ID,
+ privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\n/g, "\n"),
+ clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
 };
 
 if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-  });
+ admin.initializeApp({
+   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+   storageBucket: "pet-match-cloud.appspot.com", 
+ });
 }
 
 export const db = admin.firestore();
-export default admin; 
+export default admin;
